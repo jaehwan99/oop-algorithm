16d37fa3bc035bdaf8b744d3385fa017b1dcc0a9
jaewhan99 <bestmong0610@gmail.com>
2023-05-06 20:44:45 +0900

    - c# 5월 첫째주 코딩테스트

---
+++ bestmong/level_1/202305_1/RandomKeyboard.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Coding.Test.Level1
+{
+    public class RandomKeyboard
+    {
+        public int[] solution(string[] keymap, string[] targets)
+        {
+            int[] answer = new int[targets.Length];
+            var map = new Dictionary<char, int>();
+            var index = 0;
+
+            foreach (var key in keymap)
+            {
+                var temp = key.ToCharArray();
+                index = 1;
+                foreach (var i in temp)
+                {
+                    if (map.ContainsKey(i) == false || map[i] > index)
+                    {
+                        map[i] = index;
+                    }
+                    index++;
+                }
+            }
+
+            index = 0;
+            foreach (var target in targets)
+            {
+                var temp = target.ToCharArray();
+                var count = 0;
+                foreach (var i in temp)
+                {
+                    if (map.ContainsKey(i) == false)
+                    {
+                        count = -1;
+                        break;
+                    }
+                    else
+                        count += map[i];
+                }
+                answer[index] = count;
+                index++;
+            }
+
+            return answer;
+        }
+
+        public int[] Init()
+        {
+            // var keyArray = new string[] { "ABACD", "BCEFD" };
+            // var targets = new string[] { "ABCD", "AABB" };
+
+            var keyArray = new string[] { "AGZ", "BSSS" };
+            var targets = new string[] { "ASA", "BGZ" };
+            return solution(keyArray, targets);
+        }
+    }
+}
