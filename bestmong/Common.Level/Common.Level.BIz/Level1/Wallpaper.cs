5c8361f34dbd8f8b8593d7ad8ff63620445493a4
jaehwan99 <bestmong0610@gmail.com>
2023-05-07 18:43:19 +0900

    - .net project 생성

---
+++ bestmong/Common.Level/Common.Level.BIz/Level1/Wallpaper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Level.BIz.Level1
+{
+    public class Wallpaper
+    {
+        public int[] solution(string[] wallpaper)
+        {
+            var top = 0;
+            var left = wallpaper.Length;
+            var right = 0;
+            var bottom = 0;
+
+            var index = 0;
+            var searchChar = "#";
+            var currentValue = 0;
+            foreach (var val in wallpaper)
+            {
+                currentValue = val.LastIndexOf(searchChar);
+                if (currentValue > -1)
+                {
+                    top = index - top < 0 ? 0 : index - top;
+
+                    if (left > currentValue)
+                        left = currentValue;
+
+                    if (right <= currentValue)
+                        right = currentValue + 1;
+
+                    bottom = index + 1;
+                }
+                index++;
+            }
+
+            int[] answer = new int[] { top, left, bottom, right };
+            return answer;
+        }
+    }
+}
