ac8f08ecbe2e794ecaee00c5a571fae307da3b96
masinogns <masinogns@gmail.com>
2023-05-13 23:17:32 +0900

    뒤에있는 큰수찾기 풀이 while문을 이용한 리스트와 조건문 작성 법에 대한 이해 필요

--- masinogns/202305_02/level_2_뒤에있는큰수찾기.py
+++ masinogns/202305_02/level_2_뒤에있는큰수찾기.py
@@ -1,5 +1,4 @@
 from typing import List
-
 """
 global answer
 answer = []
@@ -26,11 +25,51 @@ def solution(numbers:List) -> List:
     return answer
 """
 
-
-def solution(numbers:List) -> List:
+""" 시간초과 , acc : 56%, list.pop(0)의 시간복잡도 O(n)임.
+def solution(numbers:List):
     answer = []
+    
+    checker =  None
+    for i in range(len(numbers)):
+        checker = numbers.pop(0)
+        # print(i, checker, numbers)
+
+        ans = None
+        for n in numbers:
+            if checker < n:
+                ans = n 
+                break
+            else:
+                ans = -1 
+        # case list is empty 
+        if ans is None:
+            ans = -1
+            
+        answer.append(ans)
+ 
+    # print(answer)
+    return answer
+"""
+
+
+def solution(numbers: List):
+    # 값 인덱스 접근을 위한 초기화 
+    answer = [-1]  * len(numbers)
+    stack = []
+    
+    for index, n in enumerate(numbers):
+        # print(index, n, stack, numbers)
+        
+        # 리스트가 비어있으면 False, 값이 하나라도 있으면 True
+        # numbers[stack[-1]]은 처음에 진행할 수 없어서 stack 조건을 함께 넣음 
+        while stack and n > numbers[stack[-1]]:
+            # print(":: ", n, numbers[stack[-1]], stack, numbers)
+            answer[stack.pop()] = n
+
+        stack.append(index)
+
     return answer
 
 if __name__ == '__main__':
     assert [3, 5, 5, -1] == solution(numbers=[2, 3, 3, 5])
-    # assert [-1, 5, 6, 6, -1, -1] == solution(numbers=[9, 1, 5, 3, 6, 2])
+    assert [-1, 5, 6, 6, -1, -1] == solution(numbers=[9, 1, 5, 3, 6, 2])
